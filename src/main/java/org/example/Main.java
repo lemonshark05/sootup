9b5573206f412b00bb4704360c749de17354a1ea
zhimin meng <mengzhimin5@gmail.com>
2024-04-10 15:22:56 -0700

    HelloWorld example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,42 +1,43 @@
 package org.example;
 
-import com.ibm.wala.cast.java.loader.JavaSourceLoaderImpl;
-import sootup.core.Language;
-import sootup.core.Project;
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.signatures.MethodSignature;
-import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
+import sootup.core.types.ClassType;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.JavaSootMethod;
+import sootup.java.core.language.JavaLanguage;
 import sootup.java.core.types.JavaClassType;
 import sootup.java.core.views.JavaView;
+import sootup.java.sourcecode.inputlocation.JavaSourcePathAnalysisInputLocation;
 
 import java.util.Collections;
 import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        AnalysisInputLocation<JavaSootClass> inputLocation =
-                new JavaClassPathAnalysisInputLocation("src/test/resources/simple/");
+        AnalysisInputLocation inputLocation =
+                new JavaSourcePathAnalysisInputLocation("src/test/resources/simple/");
 
-        JavaView view = new JavaView((Project<JavaSootClass, ? extends JavaView>) inputLocation);
-
-        JavaClassType classType = (JavaClassType) view.getIdentifierFactory().getClassType("HelloWorld");
+        JavaView view = new JavaView(inputLocation);
+        JavaClassType classType = view.getIdentifierFactory().getClassType("HelloWorld");
         Optional<JavaSootClass> classOptional = view.getClass(classType);
         if(!classOptional.isPresent()) {
-            System.out.println("Class not found");
+            System.out.println("Class not founc");
             return;
         }
 
         JavaSootClass sootClass = classOptional.get();
         MethodSignature methodSignature = view.getIdentifierFactory()
-                .getMethodSignature(classType,"main",
+                .getMethodSignature(classType, "main",
                         "void", Collections.singletonList("java.lang.String[]"));
-        Optional<JavaSootMethod> method = (Optional<JavaSootMethod>) view.getMethod(methodSignature);
+        Optional<JavaSootMethod> method = view.getMethod(methodSignature);
         if (!method.isPresent()){
             System.out.println("Method not found");
+            return;
         }
 
-        JavaSootMethod sootMethod
+        JavaSootMethod sootMethod = method.get();
+        System.out.println(sootMethod.getSignature());
+        System.out.println(sootMethod.getBody());
     }
 }

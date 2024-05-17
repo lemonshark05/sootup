c4862422e095a33c79f2377046932698d3edb612
zhimin meng <mengzhimin5@gmail.com>
2024-05-16 20:34:09 -0700

    Add PTA about FieldRef

--- src/main/java/org/example/Taints.java
+++ src/main/java/org/example/Taints.java
@@ -3,6 +3,8 @@ package org.example;
 import soot.*;
 import soot.jimple.*;
 import soot.jimple.internal.JAssignStmt;
+import soot.jimple.spark.pag.PAG;
+import soot.jimple.spark.sets.HashPointsToSet;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Edge;
 import soot.toolkits.graph.ExceptionalUnitGraph;
@@ -51,8 +53,9 @@ public class Taints {
         }
     }
     public static void main(String[] args) {
-        SootConfig.setupSoot("org.example.Demo2");
-        SootClass sootClass = Scene.v().loadClassAndSupport("org.example.Demo2");
+        String inputRoute = "org.example.Demo4";
+        SootConfig.setupSoot(inputRoute);
+        SootClass sootClass = Scene.v().loadClassAndSupport(inputRoute);
         sootClass.setApplicationClass();
         Scene.v().setMainClass(sootClass);
         printJimple(sootClass);
@@ -119,6 +122,7 @@ public class Taints {
         @Override
         protected void flowThrough(FlowSet in, Unit unit, FlowSet out) {
             in.copy(out);
+//            System.out.println("Processing unit: " + unit);
 
             if (unit instanceof JAssignStmt) {
                 JAssignStmt stmt = (JAssignStmt) unit;
@@ -127,24 +131,32 @@ public class Taints {
 
 //                System.out.println("Processing unit: " + unit);
 //                System.out.println("Input taint set: " + in);
+//                System.out.println("Left Operand: " + leftOp + " of type " + leftOp.getType() + "Is left Operand a Local? " + (leftOp instanceof Local));
+//                System.out.println("Right Operand: " + rightOp + " of type " + rightOp.getType() + "Is Right Operand a Local? " + (rightOp instanceof FieldRef));
 
-                if (rightOp instanceof Local && leftOp instanceof Local) {
-                    // Use PTA
+                // Handle assignment involving FieldRefs
+                if (leftOp instanceof FieldRef || rightOp instanceof FieldRef) {
+                    handleFieldAssignment(leftOp, rightOp, in, out);
+                }
+
+                if (leftOp instanceof Local && rightOp instanceof Local) {
                     PointsToSet rightPts = pta.reachingObjects((Local) rightOp);
                     PointsToSet leftPts = pta.reachingObjects((Local) leftOp);
-
                     if (!rightPts.isEmpty() && rightPts.hasNonEmptyIntersection(leftPts)) {
-                        // If two variables may point to the same object, pass taint
                         out.add(leftOp);
                         analyzedValues.add(stmt);
-                        System.out.println("Alias detected and taint propagated due to pointer analysis.");
+                        System.out.println("Alias detected between " + leftOp + " and " + rightOp);
                     }
                 }
 
-                // Check and handle rightOp if it's an instance of StaticFieldRef
-                if (rightOp instanceof StaticFieldRef) {
-                    // Handle static field reference specifically
-                    // Maybe check if it's a source of taint or something similar
+                // Propagation logic for static fields and constructors
+                if (rightOp instanceof StaticFieldRef || rightOp instanceof NewExpr) {
+                    // Handle static fields or new expressions as sources
+                    if (taintSource(rightOp)) {
+                        out.add(leftOp);
+                        analyzedValues.add(stmt);
+                        System.out.println("New source of taint due to " + rightOp);
+                    }
                 }
 
                 // Propagate taint if the right side is already tainted
@@ -176,19 +188,6 @@ public class Taints {
                     }
                 }
 
-                if (unit instanceof InvokeStmt) {
-                    InvokeStmt invokeStmt = (InvokeStmt) unit;
-                    InvokeExpr invokeExpr = invokeStmt.getInvokeExpr();
-                    handleMethodInvocation(invokeExpr, in, out);
-                }
-
-                if (unit instanceof ReturnStmt) {
-                    ReturnStmt returnStmt = (ReturnStmt) unit;
-                    Value returnValue = returnStmt.getOp();
-                    if (in.contains(returnValue)) {
-
-                    }
-                }
 
                 // Check if the leftOp points to any object that rightOp points to (aliasing)
                 if (rightOp instanceof Local && in.contains(rightOp)) {
@@ -217,10 +216,17 @@ public class Taints {
 
                 // Check and mark the sources
                 if (isSource(rightOp)) {
+                    System.out.println("Source detected: " + rightOp);
                     out.add(leftOp);
                     analyzedValues.add(stmt);
                     System.out.println("Source identified and tainted: " + leftOp);
                 }
+            } else if (unit instanceof InvokeStmt) {
+                InvokeStmt invokeStmt = (InvokeStmt) unit;
+                InvokeExpr invokeExpr = invokeStmt.getInvokeExpr();
+                handleMethodInvocation(invokeExpr, in, out);
+            } else if (unit instanceof ReturnStmt) {
+
             }
 
             if (unit instanceof Stmt) {
@@ -231,24 +237,124 @@ public class Taints {
             }
         }
 
+        private void handleFieldAssignment(Value leftOp, Value rightOp, FlowSet in, FlowSet out) {
+            PointsToSet rightPts = null;
+            PointsToSet leftPts = null;
+
+            // Handle right operand
+            if (rightOp instanceof FieldRef) {
+                rightPts = getFieldPointsToSet((FieldRef) rightOp);
+            } else if (rightOp instanceof Local) {
+                rightPts = pta.reachingObjects((Local) rightOp);
+            }
+
+            // Handle left operand
+            if (leftOp instanceof FieldRef) {
+                leftPts = getFieldPointsToSet((FieldRef) leftOp);
+            } else if (leftOp instanceof Local) {
+                leftPts = pta.reachingObjects((Local) leftOp);
+            }
+
+            // Handle intersection and aliasing
+            if (rightPts != null && leftPts != null && !rightPts.isEmpty() && rightPts.hasNonEmptyIntersection(leftPts)) {
+                out.add(leftOp);
+                System.out.println("Alias detected between " + leftOp + " and " + rightOp);
+            }
+        }
+
+        private PointsToSet getFieldPointsToSet(FieldRef fieldRef) {
+            if (!(pta instanceof PAG)) {
+                throw new IllegalStateException("PointsToAnalysis is not PAG as expected.");
+            }
+            PAG pag = (PAG) pta;
+
+            PointsToSet pts;
+            if (fieldRef instanceof InstanceFieldRef) {
+                InstanceFieldRef instanceFieldRef = (InstanceFieldRef) fieldRef;
+                Value base = instanceFieldRef.getBase();
+
+                // If base is a local, fetch its points-to set.
+                if (base instanceof Local) {
+                    pts = pag.reachingObjects((Local) base, fieldRef.getField());
+                } else {
+                    pts = new HashPointsToSet(fieldRef.getType(), pag);
+                }
+            } else if (fieldRef instanceof StaticFieldRef) {
+                // Handle static fields differently if required
+                pts = new HashPointsToSet(fieldRef.getType(), pag);
+            } else {
+                pts = new HashPointsToSet(fieldRef.getType(), pag);
+            }
+
+            return pts;
+        }
+
+
+        private boolean taintSource(Value val) {
+            // Implement logic to determine if a value is a taint source
+
+            return val instanceof NewExpr;
+        }
+
         private void handleMethodInvocation(InvokeExpr invokeExpr, FlowSet in, FlowSet out) {
             SootMethod method = invokeExpr.getMethod();
+
+            if (isResourceCloseMethod(method)) {
+                return; //Check whether the method is a resource closing method, if so, do not process it
+            }
+
+            List<Value> args = invokeExpr.getArgs();
+
+            System.out.println("Method call: " + method.getSignature() + " with arguments " + args);
+
             if (methodExitMap.containsKey(method)) {
                 FlowSet methodExitSet = methodExitMap.get(method);
                 out.union(methodExitSet, out); // Update the current taint set with the taint set of the called method
             }
-            // Check for taint in arguments
-            for (Value arg : invokeExpr.getArgs()) {
+            // Propagate taint from actual parameters to formal parameters
+            boolean isArgTainted = false;
+            for (int i = 0; i < args.size(); i++) {
+                Value arg = args.get(i);
                 if (in.contains(arg)) {
-                    System.out.println("Argument tainted: " + arg);
+                    if (!methodEntryMap.containsKey(method)) {
+                        methodEntryMap.put(method, new ArraySparseSet());
+                    }
+                    methodEntryMap.get(method).add(arg);
+                    isArgTainted = true;
+                    System.out.println("Argument " + arg + " is tainted.");
+                }
+            }
+
+            if (isArgTainted) {
+                System.out.println("Arguments tainted for method " + method.getSignature());
+            }
+
+            // If the method has been analyzed and we have exit information, propagate it back
+            if (methodExitMap.containsKey(method)) {
+                FlowSet methodExitSet = methodExitMap.get(method);
+                if (invokeExpr instanceof DefinitionStmt) {
+                    DefinitionStmt def = (DefinitionStmt) invokeExpr;
+                    Value leftOp = def.getLeftOp();
+                    if (!methodExitSet.isEmpty()) {
+                        out.add(leftOp);
+                        System.out.println("Taint propagated to " + leftOp);
+                    }
+                } else {
+                    out.union(methodExitSet, out);
                 }
             }
 
         }
 
+        private boolean isResourceCloseMethod(SootMethod method) {
+            String methodName = method.getName();
+            return methodName.equals("close") && method.getDeclaringClass().getType().toString().matches("java.sql.Connection|java.util.Scanner");
+        }
+
         private void handleInvocation(Stmt stmt, FlowSet in, FlowSet out) {
             InvokeExpr invokeExpr = stmt.getInvokeExpr();
             if (isSink(stmt)) {
+                System.out.println("Sink detected: " + stmt);
                 for (Value arg : invokeExpr.getArgs()) {
                     if (in.contains(arg)) {
                         System.out.println("Detected potential SQL Injection: " + stmt);
